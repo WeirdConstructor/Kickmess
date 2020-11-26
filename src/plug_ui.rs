3a99eaa0c2fc5f64cf3c44528a7a4696157cb7d1
Weird Constructor <weirdconstructor@gmail.com>
2020-11-26 19:01:17 +0100

    refactored more of the knob drawing code

--- src/plug_ui.rs
+++ src/plug_ui.rs
@@ -72,12 +72,13 @@ const UI_ELEM_N_W   : f64 =  80.0;
 const UI_ELEM_TXT_H : f64 =  20.0;
 
 struct SegmentedKnob {
-    sc:             (f64, f64),
+    sbottom:        (f64, f64),
     s:              [(f64, f64); 9],
     arc_len:        [f64; 7],
     full_len:       f64,
     s1_len:         f64,
     s2_len:         f64,
+    radius:         f64,
 }
 
 impl SegmentedKnob {
@@ -91,7 +92,7 @@ impl SegmentedKnob {
         let mut s       = [(0.0_f64, 0.0_f64); 9];
         let mut arc_len = [0.0_f64; 7];
 
-        let sc = circle_point(radius, init_rot.to_radians());
+        let sbottom = circle_point(radius, init_rot.to_radians());
 
         s[0] = circle_point(radius, (init_rot + 10.0_f64).to_radians());
         s[1] = circle_point(radius, (init_rot + 45.0_f64).to_radians());
@@ -117,28 +118,41 @@ impl SegmentedKnob {
         arc_len[6] = (s1_len + 6.0 * s2_len) / full_len;
 
         Self {
-            sc,
+            sbottom,
             s,
             arc_len,
             full_len,
             s1_len,
             s2_len,
+            radius,
         }
     }
 
     fn get_value_rect(&self) -> (f64, f64, f64, f64) {
-        (0.0, 0.0, 0.0, 0.0)
+        let width = self.radius * 2.0;
+        ((self.sbottom.0 - self.radius).round(),
+         (self.sbottom.1 - (self.radius + UI_ELEM_TXT_H * 0.5)).round(),
+         width.round(),
+         UI_ELEM_TXT_H)
     }
 
     fn get_label_rect(&self) -> (f64, f64, f64, f64) {
-        (0.0, 0.0, 0.0, 0.0)
+        let width = self.radius * 2.5;
+        ((self.sbottom.0 - width * 0.5).round(),
+         (self.sbottom.1 + UI_BG_KNOB_STROKE).round(),
+         width.round(),
+         UI_ELEM_TXT_H)
     }
 
-    fn get_label_connect_rect(&self) -> (f64, f64, f64, f64) {
-        (0.0, 0.0, 0.0, 0.0)
+    fn get_decor_rect1(&self) -> (f64, f64, f64, f64) {
+        ((self.s[0].0      - 0.25 * UI_BG_KNOB_STROKE).round(),
+         (self.sbottom.1    - 0.5 * UI_BG_KNOB_STROKE).round(),
+         ((self.s[8].0 - self.s[0].0).abs()
+                           + 0.5 * UI_BG_KNOB_STROKE).round(),
+         UI_BG_KNOB_STROKE * 3.0)
     }
 
-    fn draw_at_center(&self, cr: &cairo::Context, x: f64, y: f64, line_w: f64, color: (f64, f64, f64), value: f64) {
+    fn draw_oct_arc(&self, cr: &cairo::Context, x: f64, y: f64, line_w: f64, color: (f64, f64, f64), value: f64) {
         cr.set_line_width(line_w);
         cr.set_source_rgb(color.0, color.1, color.2);
         let s       = &self.s;
@@ -168,18 +182,20 @@ impl SegmentedKnob {
             cr.line_to(x + s[i].0, y + s[i].1);
         }
 
-        let prev = s[next_idx - 1];
-        let last = s[next_idx];
-        let rest_len = value - prev_arc_len;
-        println!("i[{}] prev_arc_len={:1.3}, rest_len={:1.3}, value={:1.3}, seglen={:1.3}",
-                 next_idx, prev_arc_len, rest_len, value,
-                 segment_len / self.full_len);
+        let prev       = s[next_idx - 1];
+        let last       = s[next_idx];
+        let rest_len   = value - prev_arc_len;
         let rest_ratio = rest_len / (segment_len / self.full_len);
+//        println!("i[{}] prev_arc_len={:1.3}, rest_len={:1.3}, value={:1.3}, seglen={:1.3}",
+//                 next_idx, prev_arc_len, rest_len, value,
+//                 segment_len / self.full_len);
         let partial =
             ((last.0 - prev.0) * rest_ratio,
              (last.1 - prev.1) * rest_ratio);
 
-        cr.line_to(x + prev.0 + partial.0, y + prev.1 + partial.1);
+        cr.line_to(
+            x + prev.0 + partial.0,
+            y + prev.1 + partial.1);
 
         cr.stroke();
     }
@@ -213,7 +229,8 @@ impl UIDrawCache {
             knob_element_short_len,
             knob_s_element_norm_len,
             knob_s_element_short_len,
-            knob: SegmentedKnob::new(UI_KNOB_RADIUS),
+            knob:   SegmentedKnob::new(UI_KNOB_RADIUS),
+            knob_s: SegmentedKnob::new(UI_KNOB_RADIUS * 0.8),
         }
     }
 
@@ -228,120 +245,72 @@ impl UIDrawCache {
                 UI_ELEM_N_H as i32,
                 UI_ELEM_N_H as i32).expect("Createable new img surface");
             self.surf[DrawCacheImg::Knob as usize] = Some(surf);
+            let cr =
+                cairo::Context::new(
+                    self.surf[DrawCacheImg::Knob as usize].as_mut().unwrap());
 
             cr.save();
-            let init_rot : f64 = 90.;
-            // middle of the new surface
 
-            let c_bottom   = circle_point(UI_KNOB_RADIUS, (init_rot).to_radians());
-
-            let (cx1, cy1) = circle_point(UI_KNOB_RADIUS, (init_rot + 10.0_f64).to_radians());
-            let (cx2, cy2) = circle_point(UI_KNOB_RADIUS, (init_rot + 45.0_f64).to_radians());
-            let (cx3, cy3) = circle_point(UI_KNOB_RADIUS, (init_rot + 90.0_f64).to_radians());
-            let (cx4, cy4) = circle_point(UI_KNOB_RADIUS, (init_rot + 135.0_f64).to_radians());
-            let (cx5, cy5) = circle_point(UI_KNOB_RADIUS, (init_rot + 180.0_f64).to_radians());
-            let (cx6, cy6) = circle_point(UI_KNOB_RADIUS, (init_rot + 225.0_f64).to_radians());
-            let (cx7, cy7) = circle_point(UI_KNOB_RADIUS, (init_rot + 270.0_f64).to_radians());
-            let (cx8, cy8) = circle_point(UI_KNOB_RADIUS, (init_rot + 315.0_f64).to_radians());
-            let (cx9, cy9) = circle_point(UI_KNOB_RADIUS, (init_rot + 350.0_f64).to_radians());
-
-
-            let cr = cairo::Context::new(self.surf[DrawCacheImg::Knob as usize].as_mut().unwrap());
+            self.knob.draw_oct_arc(
+                &cr, xo, yo,
+                UI_BG_KNOB_STROKE,
+                (0.28, 0.28, 0.28),
+                1.0);
 
             cr.set_line_width(UI_BG_KNOB_STROKE);
             cr.set_source_rgb(0.28, 0.28, 0.28);
-            cr.move_to(xo + cx1, yo + cy1);
-            cr.line_to(xo + cx2, yo + cy2);
-            cr.line_to(xo + cx3, yo + cy3);
-            cr.line_to(xo + cx4, yo + cy4);
-            cr.line_to(xo + cx5, yo + cy5);
-            cr.line_to(xo + cx6, yo + cy6);
-            cr.line_to(xo + cx7, yo + cy7);
-            cr.line_to(xo + cx8, yo + cy8);
-            cr.line_to(xo + cx9, yo + cy9);
-            cr.stroke();
 
+            let dc1 = self.knob.get_decor_rect1();
+            cr.rectangle(xo + dc1.0, yo + dc1.1, dc1.2, dc1.3);
+
+            let valrect = self.knob.get_value_rect();
             cr.rectangle(
-                (xo + cx1) - 0.25 * UI_BG_KNOB_STROKE,
-                (yo + c_bottom.1) - 0.5 * UI_BG_KNOB_STROKE,
-                (cx9 - cx1).abs() + 0.5 * UI_BG_KNOB_STROKE,
-                UI_BG_KNOB_STROKE * 3.0);
+                valrect.0 + xo, valrect.1 + yo, valrect.2, valrect.3);
+
+            let lblrect = self.knob.get_label_rect();
+            cr.rectangle(
+                lblrect.0 + xo, lblrect.1 + yo, lblrect.2, lblrect.3);
             cr.fill();
 
-            cr.rectangle(
-                (xo + c_bottom.0) - UI_ELEM_N_W * 0.5,
-                (yo + c_bottom.1) + UI_BG_KNOB_STROKE,
-                UI_ELEM_N_W,
-                UI_ELEM_TXT_H);
-            cr.fill();
-
-            cr.set_line_width(UI_MG_KNOB_STROKE);
-            cr.set_source_rgb(
-                UI_MG_KNOB_STROKE_CLR.0,
-                UI_MG_KNOB_STROKE_CLR.1,
-                UI_MG_KNOB_STROKE_CLR.2);
-            cr.move_to(xo + cx1, yo + cy1);
-            cr.line_to(xo + cx2, yo + cy2);
-            cr.line_to(xo + cx3, yo + cy3);
-            cr.line_to(xo + cx4, yo + cy4);
-            cr.line_to(xo + cx5, yo + cy5);
-            cr.line_to(xo + cx6, yo + cy6);
-            cr.line_to(xo + cx7, yo + cy7);
-            cr.line_to(xo + cx8, yo + cy8);
-            cr.line_to(xo + cx9, yo + cy9);
-            cr.stroke();
-
-//            cr.set_line_width(UI_FG_KNOB_STROKE);
-//            cr.set_source_rgb(
-//                UI_FG_KNOB_STROKE_CLR.0,
-//                UI_FG_KNOB_STROKE_CLR.1,
-//                UI_FG_KNOB_STROKE_CLR.2);
-//            cr.move_to(xo + cx1, yo + cy1);
-//            cr.line_to(xo + cx2, yo + cy2);
-//            cr.line_to(xo + cx3, yo + cy3);
-//            cr.line_to(xo + cx4, yo + cy4);
-//            cr.line_to(xo + cx5, yo + cy5);
-//            cr.stroke();
-
-            println!("LEN: {}", ((cx1 - cx2).powf(2.0) + (cy1 - cy2).powf(2.0)).sqrt());
-            println!("LEN: {}", ((cx2 - cx3).powf(2.0) + (cy2 - cy3).powf(2.0)).sqrt());
-            println!("LEN: {}", ((cx8 - cx9).powf(2.0) + (cy8 - cy9).powf(2.0)).sqrt());
+            self.knob.draw_oct_arc(
+                &cr, xo, yo,
+                UI_MG_KNOB_STROKE,
+                UI_MG_KNOB_STROKE_CLR,
+                1.0);
         }
 
         let surf = &self.surf[DrawCacheImg::Knob as usize].as_ref().unwrap();
-
-
 
         cr.save();
         cr.set_source_surface(surf, x, y);
         cr.paint();
         cr.restore();
 
-        self.knob.draw_at_center(
+        self.knob.draw_oct_arc(
             &cr, x + 10.0, y + 210.0,
             UI_MG_KNOB_STROKE,
             UI_FG_KNOB_STROKE_CLR,
             1.0);
 
-        self.knob.draw_at_center(
+        self.knob.draw_oct_arc(
             &cr, x + xo, y + yo,
             UI_MG_KNOB_STROKE,
             UI_FG_KNOB_STROKE_CLR,
             0.1);
 
-        self.knob.draw_at_center(
+        self.knob.draw_oct_arc(
             &cr, x + 90.0, y + 210.0,
             UI_MG_KNOB_STROKE,
             UI_FG_KNOB_STROKE_CLR,
             0.5);
 
-        self.knob.draw_at_center(
+        self.knob.draw_oct_arc(
             &cr, x + 190.0, y + 210.0,
             UI_MG_KNOB_STROKE,
             UI_FG_KNOB_STROKE_CLR,
             0.4);
 
-        self.knob.draw_at_center(
+        self.knob.draw_oct_arc(
             &cr, x + 10.0, y + 290.0,
             UI_MG_KNOB_STROKE,
             UI_FG_KNOB_STROKE_CLR,
@@ -356,6 +325,7 @@ pub struct UIDrawCache {
     knob_s_element_norm_len:  f64,
     knob_s_element_short_len: f64,
     knob:                     SegmentedKnob,
+    knob_s:                   SegmentedKnob,
 }
 
 pub struct PlugUIPainter<'a, 'b> {
