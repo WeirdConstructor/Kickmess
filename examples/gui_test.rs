b750bf486eb4654bf538896a341ba40d3dc5074a
Weird Constructor <weirdconstructor@gmail.com>
2020-11-27 22:32:50 +0100

    redesigned UI API

--- examples/gui_test.rs
+++ examples/gui_test.rs
@@ -6,6 +6,60 @@ use std::sync::Mutex;
 
 use kickmessvst::proc::Param;
 
+/*
+
+UI Code Architecture:
+
+Baseview / PUGL as Event source and Display loop:
+
+    - on_frame() { ui.draw(); }
+    - on_event(event) { ui.handle(event); }
+
+The plugin defining which graphical components are where,
+and which "id" they have:
+
+    ui.add_hbox([Knobs with id], [Connections])
+
+The UI handles hover state changes.
+
+The plugin defining how value changes are interpreted:
+
+    for e in ui.recv_event() {
+        match e {
+            ValueStart(ui_id)        => {},
+            ValueEnd(ui_id)          => {},
+            ValueEnter(ui_id, new_value01) => {
+                model.change(ui2param(ui_id), new_value01);
+            },
+            Value(ui_id, offs_value01) => {
+                model.change(
+                    ui2param(ui_id),
+                    model.get(ui2param(ui_id)) + offs_value * 10.0);
+                ui.send_update_value(ui_id, model.get(ui2param(ui_id)));
+            },
+        }
+    }
+
+UI is therefore:
+    - handling events
+    - drawing knobs and converting the values to human readable
+      values according to predefined conversions (big enum :)
+    - receiving new value updates
+    - drawing/updating the screen
+    - following things are down now:
+        - the UI thread owns the UI
+        - communication with the UI happens via two way mpsc
+            - the plugin gets a PluginUIHandle
+            - the view implementor (baseview / pugl) owns the UI
+
+Further abstraction:
+    - A class that knows how to draw the UI definition with Cairo
+    - Receives:
+        - UI description
+        - Interpreted values (strings)
+
+*/
+
 pub trait UIParameterProvider : Send + Sync {
     fn get(&self, p: Param) -> f32;
     fn set(&self, p: Param, v: f32);
@@ -94,6 +148,7 @@ impl PlugUI for UI {
     }
 
     fn handle_input(&mut self, p: &mut PlugUIPainter) {
+        self.needs_redraw = 0;
     }
 }
 
